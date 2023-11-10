c589b8321752007424c8bc6ee3504e765396af85
AMINU BILAL <aminubillu@email.com>
2023-11-10 17:09:16 +0100

    update: Carousel added

--- src/component/Slideimage/Slideimage.jsx
+++ src/component/Slideimage/Slideimage.jsx
@@ -1,11 +1,32 @@
 import './Slideimage.css'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slide1 from "../../assets/AdobeStock_571400016.jpeg"
+import Slide2 from "../../assets/AdobeStock_592460071.jpeg"
+import Slide3 from "../../assets/AdobeStock_592460176.jpeg"
+import Slide4 from "../../assets/alexandre-lallemand-UtPC_kz8CAc-unsplash.jpg"
+import Slide5 from "../../assets/markus-winkler-30kISztIV8w-unsplash.jpg"
 
 const Slideimage = () => {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  
   return (
     <div className='slide'>
-      <div className="image">
-
-      </div>
+      <Slider {...settings} className="image">
+        <div><img src={Slide1} alt="Slide1" /></div>
+        <div><img src={Slide2} alt="Slide2" /></div>
+        <div><img src={Slide3} alt="Slide3" /></div>
+        <div><img src={Slide4} alt="Slide4" /></div>
+        <div><img src={Slide5} alt="Slide5" /></div>
+      </Slider>
     </div>
   )
 }
